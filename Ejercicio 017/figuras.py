cecf994ba30ed70bceb3225052fe4bb600171551
Santiago Sarmiento <imlunaaa05@gmail.com>
2026-03-26 23:05:05 -0500

    Update figuras.py

--- Ejercicio 017/figuras.py
+++ Ejercicio 017/figuras.py
@@ -1,24 +1,63 @@
-class FiguraGrafica:
-    def dibujar(self):
-        pass
+
+class FiguraGeometrica:
+
+    def __init__(self, nombre, lados, angulos):
+        self.nombre = nombre
+        self.lados = lados
+        self.angulos = angulos
+
+    def mostrar_info(self):
+        print("Figura:", self.nombre)
+        print("Lados:", self.lados)
+        print("Ángulos:", self.angulos)
 
 
-class Circulo(FiguraGrafica):
-    def dibujar(self):
-        print("Dibujando círculo")
+
+class Triangulo(FiguraGeometrica):
+
+    def __init__(self, base, altura):
+        super().__init__("Triangulo", 3, 3)
+        self.base = base
+        self.altura = altura
+
+    def calcular_area(self):
+        return (self.base * self.altura) / 2
 
 
-class Rectangulo(FiguraGrafica):
-    def dibujar(self):
-        print("Dibujando rectángulo")
+
+class Cuadrado(FiguraGeometrica):
+
+    def __init__(self, lado):
+        super().__init__("Cuadrado", 4, 4)
+        self.lado = lado
+
+    def calcular_area(self):
+        return self.lado ** 2
 
 
-class Linea(FiguraGrafica):
-    def dibujar(self):
-        print("Dibujando línea")
+class Circulo(FiguraGeometrica):
+
+    def __init__(self, radio):
+        super().__init__("Circulo", 0, 0)
+        self.radio = radio
+
+    def calcular_area(self):
+        import math
+        return math.pi * self.radio ** 2
 
 
-figuras=[Circulo(),Rectangulo(),Linea()]
 
-for f in figuras:
-    f.dibujar()
+t = Triangulo(6,4)
+c = Cuadrado(5)
+ci = Circulo(3)
+
+t.mostrar_info()
+print("Área:", t.calcular_area())
+print()
+
+c.mostrar_info()
+print("Área:", c.calcular_area())
+print()
+
+ci.mostrar_info()
+print("Área:", ci.calcular_area())
