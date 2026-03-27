d422ce06a9e3a27775b39263fd26ab709519f7d8
Santiago Sarmiento <imlunaaa05@gmail.com>
2026-03-26 22:58:42 -0500

    Update sistemaAuto.py

--- Ejercicio 015/sistemaAuto.py
+++ Ejercicio 015/sistemaAuto.py
@@ -1,12 +1,11 @@
 class Vehiculo:
-    def __init__(self, marca, modelo, velocidad):
-        self.marca = marca
-        self.modelo = modelo
-        self.velocidad = velocidad
+    def __init__(self,marca,modelo,velocidad):
+        self.marca=marca
+        self.modelo=modelo
+        self.velocidad=velocidad
 
     def info(self):
-        print(self.marca, self.modelo,
-              "Velocidad:", self.velocidad)
+        print(self.marca,self.modelo,"Velocidad:",self.velocidad)
 
     def funcionamiento(self):
         pass
@@ -14,14 +13,28 @@ class Vehiculo:
 
 class Carro(Vehiculo):
     def funcionamiento(self):
-        print("Vehículo familiar de uso urbano")
+        print("Uso urbano familiar")
 
 
 class Moto(Vehiculo):
     def funcionamiento(self):
-        print("Vehículo ágil de dos ruedas")
+        print("Vehículo de dos ruedas")
 
 
 class Camion(Vehiculo):
     def funcionamiento(self):
-        print("Vehículo pesado de carga")
+        print("Transporte de carga pesada")
+
+
+
+lista=[
+    Carro("Toyota","Corolla",180),
+    Moto("Yamaha","R15",150),
+    Camion("Volvo","FH",120)
+]
+
+for v in lista:
+    print("Tipo:",type(v).__name__)
+    v.info()
+    v.funcionamiento()
+    print("-----")
